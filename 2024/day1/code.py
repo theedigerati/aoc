8ecc4faf363b571841db6dd7adf258478dceb23a
theedigerati <39467790+theedigerati@users.noreply.github.com>
2024-12-07 14:28:30 +0100

    update python code for day1 2024

--- 2024/day1/code.py
+++ 2024/day1/code.py
@@ -1,26 +1,19 @@
-import re
-
 list1 = []
 list2 = []
-length = 0
 with open("input.txt") as lists:
     for line in lists:
-        nums = re.split("\W+", line)
+        nums = line.split()
         list1.append(int(nums[0]))
         list2.append(int(nums[1]))
-        length += 1
 
 list1.sort()
 list2.sort()
 
-total_distance = 0
-for i in range(length):
-    total_distance += abs(list1[i] - list2[i])
+distance = 0
+similarity = 0
+for i in range(len(list1)):
+    distance += abs(list1[i] - list2[i])
+    similarity += list1[i] * list2.count(list1[i])
 
-print("Total distance: ", total_distance)
-
-similiarity_score = 0
-for num in list1:
-    similiarity_score += num * list2.count(num)
-
-print("Similiarity score: ", similiarity_score)
+print("Total distance: ", distance)
+print("Similiarity score: ", similarity)
