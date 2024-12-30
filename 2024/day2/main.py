8620f8f3c09dcfa6ec7dd1abb1a454fa6bebb300
theedigerati <39467790+theedigerati@users.noreply.github.com>
2024-12-30 20:11:50 +0100

    update py code for day2 2024

--- 2024/day2/main.py
+++ 2024/day2/main.py
@@ -15,9 +15,7 @@ def validate(levels, use_dampner=True):
         diff_state, valid = validate_level(diff_state, a, b)
         if valid:
             continue
-        elif use_dampner is False:
-            break
-        elif dampner is not None:
+        elif use_dampner is False or dampner is not None:
             break
         elif i == 0:
             dampner = i
@@ -33,16 +31,14 @@ def validate(levels, use_dampner=True):
         if valid:
             dampner = i
             continue
-        else:
-            a = int(levels[i])
-            b = int(levels[i + 2])
-            _, valid = validate_level(diff_state, a, b)
-            if valid:
-                dampner = i + 1
-                continue
-            else:
-                # at this point we've tried 2 dampners that failed
-                break
+        a = int(levels[i])
+        b = int(levels[i + 2])
+        _, valid = validate_level(diff_state, a, b)
+        if valid:
+            dampner = i + 1
+            continue
+        # at this point we've tried 2 dampners that failed
+        break
     return valid
 
 
@@ -66,12 +62,13 @@ with open("input.txt") as reports:
 
         if validate(levels, use_dampner=False):
             safe_reports += 1
-        elif validate(levels):
+            continue
+        if validate(levels):
             safe_reports_with_dampner += 1
-        else:
-            levels.reverse()
-            if validate(levels):
-                safe_reports_with_dampner += 1
+            continue
+        levels.reverse()
+        if validate(levels):
+            safe_reports_with_dampner += 1
 
-print("Total safe reports: ", safe_reports)
-print("Total safe reports with dampner: ", safe_reports + safe_reports_with_dampner)
+print("Total safe reports:", safe_reports)
+print("Total safe reports with dampner:", safe_reports + safe_reports_with_dampner)
